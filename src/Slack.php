fe3fb7fcb90e8cc81aa7c1045f9581e5e33caade
Djalma Manfrin <djalma.manfrin@madeiramadeira.com.br>
2019-11-05 13:28:59 -0300

    clear service

--- src/Slack.php
+++ src/Slack.php
@@ -7,18 +7,9 @@ use MM\Notifier\Factories\MessageFactory;
 
 class Slack
 {
-    /**
-     * The Slack incoming webhook endpoint.
-     *
-     * @var string
-     */
+    /** @var string */
     protected $endpoint;
 
-    /**
-     * Instantiate a new Slack.
-     *
-     * @param string $endpoint
-     */
     public function __construct($endpoint)
     {
         $this->endpoint = $endpoint;
@@ -26,9 +17,6 @@ class Slack
 
     public function send(Message $message):bool
     {
-        // ok
-        // invalid_payload
-        //
         try {
             $json = json_encode(MessageFactory::toArray($message));
             $command = "curl -X POST -H 'Content-type: application/json' --data '";
