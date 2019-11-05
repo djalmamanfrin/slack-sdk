a7a78cd0859fac6eadd1ad5a24f772def3e5709b
Djalma Manfrin <djalma.manfrin@madeiramadeira.com.br>
2019-11-05 09:48:39 -0300

    tests struture

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('ROOT_DIR')) {
+    define('ROOT_DIR', __DIR__);
+}
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->overload();
