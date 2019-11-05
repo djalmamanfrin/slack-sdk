faef853c8da094aee0255cccf0df502f952bc5ac
Djalma Manfrin <djalma.manfrin@madeiramadeira.com.br>
2019-11-05 09:45:44 -0300

    Enums

---
+++ src/Enums/ButtonTypeEnum.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MM\Notifier\Enums;
+
+use InvalidArgumentException;
+use ReflectionClass;
+
+class ButtonTypeEnum
+{
+    const BUTTON = 'button';
+    const DEFAULT = 'default';
+    const PRIMARY = 'primary';
+    const DANGER = 'danger';
+
+    public static function all()
+    {
+        $reflector = new ReflectionClass(__CLASS__);
+        return $reflector->getConstants();
+    }
+
+    public static function validate($type)
+    {
+        return array_key_exists(strtoupper($type), self::all());
+    }
+}
---
+++ src/Enums/IconTypeEnum.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MM\Notifier\Enums;
+
+use InvalidArgumentException;
+use ReflectionClass;
+
+class IconTypeEnum
+{
+    const ICON_URL = 'icon_url';
+    const ICON_EMOJI = 'icon_emoji';
+
+    public static function all()
+    {
+        $reflector = new ReflectionClass(__CLASS__);
+        return $reflector->getConstants();
+    }
+
+    public static function validate($type)
+    {
+        if (!array_key_exists(strtoupper($type), self::all())) {
+            $message = "The iconType field is not found in IconTypeEnum class. So, the type informed (%s) is not allowed";
+            throw new InvalidArgumentException(sprintf($message, $type));
+        }
+    }
+}
